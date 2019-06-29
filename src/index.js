853fe889f27fe04539ff2d3620352901c6ee8846
Robert Laws <roblaws@me.com>
2019-06-29 14:02:59 +0300

    finish props section

--- src/index.js
+++ src/index.js
@@ -1,27 +1,46 @@
 // import react and react-dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // Create a react component
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Bob
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:30PM</span>
-          </div>
-          <div className="text">
-            Nice blog post.
-          </div>
-        </div>
-      </div>
+      <ApprovalCard>Are you sure?</ApprovalCard>
+      
+      <ApprovalCard>
+        <h4>Warning!</h4>
+        Are you sure about this?
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail 
+          avatarImage={faker.image.avatar()} 
+          author="Sam" 
+          postTime="Today at 11AM" 
+          commentText="Nice blog post" 
+        />
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail 
+          avatarImage={faker.image.avatar()} 
+          author="Chris" 
+          ostTime="Yesterday at 5:30PM" 
+          commentText="Interesting ideas..." 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          avatarImage={faker.image.avatar()} 
+          author="Alex" 
+          postTime="Today at 9PM" 
+          commentText="Needs more detail"
+        />
+      </ApprovalCard>
     </div>
   )
 };
@@ -31,6 +50,3 @@ ReactDOM.render(
   <App />, 
   document.querySelector("#root")
 )
-
-
-
