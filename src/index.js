87bbfc8dfcc88d9fa835ad81049048b7f6a559de
Robert Laws <roblaws@me.com>
2019-06-29 20:24:25 +0300

    implement simple latitude or error message

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 // import react and react-dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+// import SeasonDisplay from './SeasonDisplay';
 
 
 // Create a react component
-const App = () => {
-  return <div>
-    hello
-    <SeasonDisplay />
-  </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude
+        });
+      },
+      (err) => {
+        this.setState({
+          errorMessage: err.message
+        })
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>
+  }
 }
 
 // Take the react component and show on screen
