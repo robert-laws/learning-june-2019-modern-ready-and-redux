e062e48a32d8feca028a68695fa2df8b44e26c1e
Robert Laws <roblaws@me.com>
2019-06-29 17:49:07 +0300

    begin class-based component section

--- src/index.js
+++ src/index.js
@@ -1,48 +1,16 @@
 // import react and react-dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
+import SeasonDisplay from './SeasonDisplay';
+
 
 // Create a react component
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>Are you sure?</ApprovalCard>
-
-      <ApprovalCard>
-        <h4>Warning!</h4>
-        Are you sure about this?
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail 
-          avatarImage={faker.image.avatar()} 
-          author="Sam" 
-          postTime="Today at 11AM" 
-          commentText="Nice blog post" 
-        />
-      </ApprovalCard>
-      
-      <ApprovalCard>
-        <CommentDetail 
-          avatarImage={faker.image.avatar()} 
-          postTime="Yesterday at 5:30PM" 
-          commentText="Interesting ideas..." 
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail 
-          avatarImage={faker.image.avatar()} 
-          author="Alex" 
-          postTime="Today at 9PM" 
-          commentText="Needs more detail"
-        />
-      </ApprovalCard>
-    </div>
-  )
-};
+  return <div>
+    hello
+    <SeasonDisplay />
+  </div>
+}
 
 // Take the react component and show on screen
 ReactDOM.render(
